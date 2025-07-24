c5bc6a3772ee9acb15407e3cf99f398e1404724b
nandanugg <nandanugg@gmail.com>
2025-07-25 02:16:16 +0700

    update

--- lua/plugins/session.lua
+++ lua/plugins/session.lua
@@ -1,7 +1,9 @@
 -- session.lua contain configurations for code sessions
 -- > SESSION
 require("auto-session").setup({
-    cwd_change_handling = false,
+    auto_restore_last_session = true,
+    show_auto_restore_notif = true,
+    lsp_stop_on_restore = true,
 })
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,localoptions"
 -- < SESSION
